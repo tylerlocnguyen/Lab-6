5f2054d7c964c97748230bc76e06564e8fa26d6b
tylernguyen <tylerlocnguyen@gmail.com>
2023-03-22 23:07:13 -0400

    oops

--- lab6.py
+++ lab6.py
@@ -1,11 +1,13 @@
 def encoder(integer):
+    global result
     result = ""
     encode_list = []
     encode_list[:0] = integer
     encode_list = [int(j) + 3 for j in encode_list]
     for j in encode_list:
         result += str(j)
-    print(result)
+    print(f'Your password has been encoded and stored!')
+    return result
     pass
 def decoder(Input):
     pass
@@ -20,6 +22,8 @@ def main():
         if option == "1":
             encode = input(f'Please enter your password to encode: ')
             encoder(encode)
+        if option == "2":
+            decoder(result)
         if option == "3":
             break
 if __name__ == '__main__':
