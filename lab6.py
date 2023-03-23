5cdb87b0b93fe2a52746b5b49e74887a083dda87
tiffanywei546 <tiffany.wei546@gmail.com>
2023-03-22 23:20:25 -0400

    added decode!!!

--- lab6.py
+++ lab6.py
@@ -9,8 +9,18 @@ def encoder(integer):
     print(f'Your password has been encoded and stored!')
     return result
     pass
-def decoder(Input):
-    pass
+def decode(code):
+    list1=[int(x) for x in code]
+    list2=[]
+    for num in list1:
+        num=num-3
+        list2.append(num)
+    final_decode=''
+    for num in list2:
+        num=str(num)
+        final_decode+=num
+    return final_decode
+
 def main():
     while True:
         option = input(f'Menu\n'
@@ -21,9 +31,9 @@ def main():
             f'Please enter an option: ')
         if option == "1":
             encode = input(f'Please enter your password to encode: ')
-            encoder(encode)
+            code=encoder(encode)
         if option == "2":
-            decoder(result)
+            print(f'The encoded password is {code}, and the original password is {decode(code)}.')
         if option == "3":
             break
 if __name__ == '__main__':
